8ce26ef7b37f275aa28e1f0dde904a46c97e9c0c
zhangyang <zhangyang_2002@foxmail.com>
2023-11-21 23:14:38 +0800

    备份

--- jdk20-start-study/src/main/java/org/example/Main.java
+++ jdk20-start-study/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
@@ -10,26 +11,25 @@ import java.util.concurrent.Executors;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        Instant start = Instant.now();
-
-        try (var executor = Executors.newFixedThreadPool(100)) {
-            for (int i = 0; i < 100000; i++) {
-                executor.submit(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            // 模拟业务时间
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                });
-            }
-        }
-        Instant finish = Instant.now();
-        long timeElapsed = Duration.between(start, finish).toMillis();
-        System.out.println("总耗时 : " + timeElapsed);
-    }
+//    public static void main(String[] args) {
+//        Instant start = Instant.now();
+//        try (var executor = Executors.newFixedThreadPool(100)) {
+//            for (int i = 0; i < 100000; i++) {
+//                executor.submit(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        try {
+//                            // 模拟业务时间
+//                            Thread.sleep(1000);
+//                        } catch (InterruptedException e) {
+//                            throw new RuntimeException(e);
+//                        }
+//                    }
+//                });
+//            }
+//        }
+//        Instant finish = Instant.now();
+//        long timeElapsed = Duration.between(start, finish).toMillis();
+//        System.out.println("总耗时 : " + timeElapsed);
+//    }
 }
